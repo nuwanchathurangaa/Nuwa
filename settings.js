fa7f454f6f1e04c7dda87a3880d4d659a6a736e0
nuwanchathurangaa <165279317+nuwanchathurangaa@users.noreply.github.com>
2024-07-06 15:04:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bQE12JKQ#uBHTU4diUdI78IRxqDYIS3DXQwZUqONYfUxSDTeIB3k' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=GRkx0QIQ#DgOQGhXRNYfWw58jWOh1KU2rxjkb2F1iDTSHO7L8MW0 ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
